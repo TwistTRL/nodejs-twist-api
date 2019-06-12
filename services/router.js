d19d3d6301d36a20b7324d9b419d0dc4a34fb831
Lingyu Zhou <zhoulytwin@gmail.com>
2019-06-12 17:01:34 -0400

    API template freeze.

--- services/router.js
+++ services/router.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = new express.Router();
-const RSSRouter = require('../routes/RSS');
+var database = require("../services/database");
 
-router.use('/RSS', RSSRouter);
+const baseQuery =
+ `SELECT *
+  FROM RSS
+  WHERE person_ID = :person_ID
+ `;
+
+router.get('/person/:person_ID/RSS', async function(req, res, next) {
+  let binds = {person_ID: req.params.person_ID};
+  let result = await database.simpleExecute(baseQuery, binds);
+  res.send(result.rows);
+});
 
 module.exports = router;
