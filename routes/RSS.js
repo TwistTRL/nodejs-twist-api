8b262cd0a51278235dccce4771e460605274b760
Lingyu Zhou <zhoulytwin@gmail.com>
2019-06-20 15:31:06 -0400

    Changed RSS SQL

--- routes/RSS.js
+++ routes/RSS.js
@@ -3,9 +3,10 @@ const router = new express.Router({mergeParams: true});
 const database = require("../services/database");
 
 const baseQuery =
- `SELECT *
+ `SELECT RSS as RespiratorySupportScore, VALID_FROM_DT_TM*1000 AS time
   FROM RSS
   WHERE person_ID = :person_ID
+  ORDER BY time ASC
  `;
 
 router.get('/',async function(req, res, next) {
