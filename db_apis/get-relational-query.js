56055587d8e5a3cb0bb029fbaee93faef6013dd8
Peng <pzeng123@gmail.com>
2019-12-03 15:00:52 -0500

    code from twist server Nov 21

--- db_apis/get-relational-query.js
+++ db_apis/get-relational-query.js
@@ -5,11 +5,10 @@ async function relationalQuerySQLExecutor(conn,query){
   let ret = {};
   let querySQL =  sqlBuilder.buildSQL(query);
   for (let [entity,[sql,binds]] of Object.entries(querySQL)) {
-    console.log(sql,binds);
+    console.log(entity,'\n',sql,binds);
     let t = Date.now();
     let res = await conn.execute(sql,binds).then( res=>res.rows );
     ret[entity] = res;
-    console.log((Date.now()-t)/1000);
   }
   return ret;
 }
