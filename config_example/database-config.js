44931422da85afa393655f9e9628b60f79e97bca
pzeng123 <pzeng123@gmail.com>
2019-12-13 14:15:57 -0500

    add config_example

---
+++ config_example/database-config.js
@@ -0,0 +1,12 @@
+let user = "";
+let password = "";
+let connectString = "";
+
+module.exports = {
+  user,
+  password,
+  connectString,
+  poolMin: 10,
+  poolMax: 10,
+  poolIncrement: 0
+};
---
+++ config_example/web-server-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: process.env.HTTP_PORT || 3000,
+  ipWhiteList : ['::ffff:127.0.0.1']
+};
