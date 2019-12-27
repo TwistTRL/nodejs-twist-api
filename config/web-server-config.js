bd31ec7b274951a62b0c2abaf2b4c35e5336b855
pzeng123 <pzeng123@gmail.com>
2019-12-27 10:57:07 -0500

    config back

---
+++ config/web-server-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: process.env.HTTP_PORT || 3300,
+  ipWhiteList : ['::ffff:10.41.13.4', '::ffff:10.7.46.37', '::ffff:10.41.13.5', '::ffff:10.41.13.6', '::ffff:10.7.46.137']
+};
--- config_example/web-server-config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  port: process.env.HTTP_PORT || 3000,
-  ipWhiteList : ['::ffff:127.0.0.1']
-};
