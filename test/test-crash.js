b6822db8fac677060476d25b01e2111382af282b
pzeng123 <pzeng123@gmail.com>
2019-12-27 11:52:01 -0500

    add dependencies

--- test/test-crash.js
+++ test/test-crash.js
@@ -2,12 +2,12 @@
  * make node js crash
  */
 
-function function_crash() {
-    process.nextTick(function () {
-        throw new Error;
-      });
-}
+// function function_crash() {
+//     process.nextTick(function () {
+//         throw new Error;
+//       });
+// }
 
 
-console.log("Crashing in 3 seconds...")
-setTimeout(function_crash, 3000);
+// console.log("Crashing in 3 seconds...")
+// setTimeout(function_crash, 3000);
