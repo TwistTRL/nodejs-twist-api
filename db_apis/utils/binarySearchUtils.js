24a3a953179344b8e5204a5ca0ebad32640e8188
pzeng123 <pzeng123@gmail.com>
2020-02-28 13:37:49 -0500

    re move weight, new structure of inout tooltip v2

--- db_apis/utils/binarySearchUtils.js
+++ db_apis/utils/binarySearchUtils.js
@@ -1,32 +1,38 @@
-
 /**
- * 
- * Recursive function to get the value index of a sorted number array smaller nearest target number  
- * @param {Number} num 
- * @param {Array} arr 
+ *
+ * Recursive function to get the value index of a sorted number array smaller nearest target number
+ * @param {Number} num
+ * @param {Array} arr
  * @returns {Number} index of arr
  */
 const getBinarySearchNearest = (num, arr) => {
-    if (!arr || !arr.length) {
-      return null;
-    }
-  
-    if (arr.length == 1) {
-      return 0;
-    }
-  
-    let mid = Math.floor((arr.length - 1) / 2);
-    if (arr[mid] == num) {
-      return mid;
-    } else if (arr[mid] > num) {
-      return getBinarySearchNearest(num, arr.slice(0, mid + 1));
-    } else {
-      return getBinarySearchNearest(num, arr.slice(mid + 1)) + mid + 1;
-    }
+  if (!arr || !arr.length) {
+    return null;
   }
 
-  
-  module.exports = {
-    getBinarySearchNearest
-  };
-  
+  if (arr.length == 1) {
+    return 0;
+  }
+
+  let mid = Math.floor((arr.length - 1) / 2);
+  if (arr[mid] == num) {
+    return mid;
+  } else if (arr[mid] > num) {
+    return getBinarySearchNearest(num, arr.slice(0, mid + 1));
+  } else {
+    return getBinarySearchNearest(num, arr.slice(mid + 1)) + mid + 1;
+  }
+};
+
+const getWeightOnTime = (timestamp, weightArray) => {
+  let timeArr = weightArray.map(item => item.DT_UNIX);
+  let index = getBinarySearchNearest(timestamp, timeArr);
+  let roundWeight =
+    Math.round((weightArray[index].WEIGHT + Number.EPSILON) * 1000) / 1000;
+  return roundWeight;
+}
+
+module.exports = {
+  getBinarySearchNearest,
+  getWeightOnTime
+};
