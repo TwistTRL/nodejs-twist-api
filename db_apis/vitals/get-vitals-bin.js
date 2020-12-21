ca48aafbd5c9e8ffff34f50bfdda6c18a51178d8
pzeng123 <pzeng123@gmail.com>
2020-12-21 15:32:27 -0500

    binned vitals response combined bin_id for the same LMT_ST and LMT_END range

--- db_apis/vitals/get-vitals-bin.js
+++ db_apis/vitals/get-vitals-bin.js
@@ -2,7 +2,7 @@
  * @Author: Peng Zeng
  * @Date: 2020-12-03 21:10:09
  * @Last Modified by: Peng Zeng
- * @Last Modified time: 2020-12-19 16:45:01
+ * @Last Modified time: 2020-12-21 15:31:03
  */
 
 const SORTED_TABLE = ["VITALS", "VITAL_V500"];
@@ -27,21 +27,29 @@ const SORTED_TWIST_NAME = [
 const getVitalsBin = ( bin_data, bin_def ) => {
   // console.log('bin_def :>> ', bin_def);
   // console.log('bin_data :>> ', bin_data);
+  const LMTST_DICT = {};
   const BIN_ID_TO_RANGE = {};
+  const BIN_ID_TO_BIN_ID = {};      // NBPS's bin_id will map to SBP1's bin_id 
   const NAME_TO_BIN_ID = {};
   const BIN_ID_TO_NAME = {};
 
   for (let row of bin_def) {
-    BIN_ID_TO_RANGE[row.BIN_ID] = [row.LMT_ST, row.LMT_END];
+    if (row.LMT_ST in LMTST_DICT) {
+      BIN_ID_TO_BIN_ID[row.BIN_ID] = LMTST_DICT[row.LMT_ST].BIN_ID;
+
+    } else {
+      BIN_ID_TO_BIN_ID[row.BIN_ID] = row.BIN_ID;
+      BIN_ID_TO_RANGE[row.BIN_ID] = [row.LMT_ST, row.LMT_END];
+      LMTST_DICT[row.LMT_ST] = row;
+    }
     BIN_ID_TO_NAME[row.BIN_ID] = row.VITAL_TYPE;
     if (row.VITAL_TYPE in NAME_TO_BIN_ID) {
       NAME_TO_BIN_ID[row.VITAL_TYPE].push(row.BIN_ID);
     } else {
       NAME_TO_BIN_ID[row.VITAL_TYPE] = [row.BIN_ID];
     }
-
   }
-
+  
   const ret = [BIN_ID_TO_RANGE];
 
   let dict = {}; // dict[start_time][source][name]
@@ -84,14 +92,18 @@ const getVitalsBin = ( bin_data, bin_def ) => {
         (x, y) => SORTED_TWIST_NAME.indexOf(x) - SORTED_TWIST_NAME.indexOf(y)
       )[0];
 
-      // fill all unused id with 0
+      // make sbp1 and nsbp the same bin_id so the same range
+      // and fill all unused id with 0
       const cur_result = dict[from_time][source_name][twist_name];
+      const ret_result = {from: cur_result.from, to: cur_result.to, time: cur_result.time, name: cur_result.name, source: cur_result.source};
       NAME_TO_BIN_ID[twist_name].forEach(binId => {
         if (!(binId in cur_result)) {
-          cur_result[binId] = 0;
+          ret_result[BIN_ID_TO_BIN_ID[binId]] = 0;
+        } else {
+          ret_result[BIN_ID_TO_BIN_ID[binId]] = cur_result[binId];
         }
       })
-      ret.push(cur_result);
+      ret.push(ret_result);
     });
 
   return ret;
--- db_relation/vitals-api.js
+++ db_relation/vitals-api.js
@@ -2,13 +2,12 @@
  * @Author: Peng Zeng 
  * @Date: 2020-12-17 11:25:51 
  * @Last Modified by: Peng Zeng
- * @Last Modified time: 2020-12-21 10:52:44
+ * @Last Modified time: 2020-12-21 14:47:00
  */
 
 const {
   VITALS_DICT,
 } = require("twist-xlsx");
-console.log('from tiwst-xlsx, VITALS_DICT :>> ', VITALS_DICT);
 module.exports = {
   VITALS_DICT,
 };
