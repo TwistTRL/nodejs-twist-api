2c107a9dcbbc920a3a976a9c2bc44f95e7aa5872
pzeng123 <pzeng123@gmail.com>
2020-12-21 11:04:50 -0500

    vitals debug - 4

--- database_access/vitals/vitals-calc.js
+++ database_access/vitals/vitals-calc.js
@@ -2,7 +2,7 @@
  * @Author: Peng Zeng
  * @Date: 2020-12-03 09:46:17
  * @Last Modified by: Peng Zeng
- * @Last Modified time: 2020-12-21 10:58:01
+ * @Last Modified time: 2020-12-21 11:04:33
  */
 
 const database = require("../../services/database");
@@ -45,24 +45,36 @@ ORDER BY START_TM`;
 async function vitalsCalcQuerySQLExecutor(conn, binds) {
   const { input_vital_type, person_id, data_resolution } = binds;
   const vital_type_dict = VITALS_DICT[input_vital_type];
-  console.log('VITALS_DICT :>> ', VITALS_DICT);
-  console.log('vital_type_dict :>> ', vital_type_dict);
-  
+  console.log("VITALS_DICT :>> ", VITALS_DICT);
+  console.log("vital_type_dict :>> ", vital_type_dict);
+
   let calc_data = [];
   for (let table_name in CALC_TABLE) {
     const calc_table = CALC_TABLE[table_name][data_resolution];
-    console.log('calc_table :>> ', calc_table);
-    console.log('Object.values(vital_type_dict) :>> ', Object.values(vital_type_dict));
-    console.log('Object.values(vital_type_dict).map(item => item[table_name]) :>> ', Object.values(vital_type_dict).map(item => item[table_name]));
+    console.log("calc_table :>> ", calc_table);
+    console.log("Object.values(vital_type_dict) :>> ", Object.values(vital_type_dict));
+    console.log(
+      "Object.values(vital_type_dict).map(item => item[table_name]) :>> ",
+      Object.values(vital_type_dict).map((item) => item[table_name])
+    );
 
-    const vital_type_list = Object.values(vital_type_dict).map(item => item[table_name]).flat().filter(Boolean);
-
+    const vital_type_list = Object.values(vital_type_dict)
+      .map((item) => item[table_name])
+      .flat()
+      .filter(Boolean);
+    console.log("vital_type_list :>> ", vital_type_list);
     if (calc_table) {
       const cur_result = await conn
-      .execute(SQL_CALC(calc_table), { person_id })
-      .then((ret) => ret.rows)
-      .then(ret => ret.filter(x => vital_type_list.includes(x.VITAL_TYPE)))
-      .then((ret) => ret.map((x) => ({ ...x, table_source: table_name, time: (Number(x.START_TM) + Number(x.END_TM)) / 2 })));
+        .execute(SQL_CALC(calc_table), { person_id })
+        .then((ret) => ret.rows)
+        .then((ret) => ret.filter((x) => vital_type_list.includes(x.VITAL_TYPE)))
+        .then((ret) =>
+          ret.map((x) => ({
+            ...x,
+            table_source: table_name,
+            time: (Number(x.START_TM) + Number(x.END_TM)) / 2,
+          }))
+        );
       calc_data = [...calc_data, ...cur_result];
     }
   }
