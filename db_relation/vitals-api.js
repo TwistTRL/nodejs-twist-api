f3b2d7c09117b159bf421b14f4c71d05f97c8ee5
pzeng123 <pzeng123@gmail.com>
2020-12-22 15:18:04 -0500

    fixed a bug vitals spo2 not display

--- db_relation/vitals-api.js
+++ db_relation/vitals-api.js
@@ -2,12 +2,16 @@
  * @Author: Peng Zeng 
  * @Date: 2020-12-17 11:25:51 
  * @Last Modified by: Peng Zeng
- * @Last Modified time: 2020-12-21 14:47:00
+ * @Last Modified time: 2020-12-22 15:00:40
  */
 
 const {
   VITALS_DICT,
 } = require("twist-xlsx");
+
+// TODO: now added SPO2 so all the keys could be captialized.
+VITALS_DICT.SPO2 = VITALS_DICT.SpO2;
+
 module.exports = {
   VITALS_DICT,
 };
